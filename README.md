37c74084413156e70c8404e7744f45fd71130957
Mashkov Andrey <andmmm@mail.ru>
2016-01-26 01:09:20 +0300

    js для навигации, исправление ошибок, README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+1. git clone https://github.com/andmegam/loft1.git loft1
+2. cd loft1
--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-      /*$(function()
-        {
-            alert('Подключена последняя версия jQuery через Google хостинг ' + jQuery.fn.jquery);
-        });*/
+/* Navigation*/
+var nav = (function () {
+	var navelements = document.getElementById('navigation-list').getElementsByTagName('li'),
+		currentpage = location.pathname.match(/[^/]*$/);
+
+	if (navelements.length>0 ) {
+        for(var i = 0, len = navelements.length;  i<len; i++) {
+
+			if (currentpage[0] === "" ) {
+				currentpage = "index.html";
+			}
+
+            if (navelements[i].querySelector('a').href.indexOf(currentpage) !=-1) {
+                navelements[i].className += " current";
+
+            }
+        }
+    }
+})();
